f698e781b143fce1632e65f56c80aa075d57b670
FrancescoMezzacasa <framezza05@gmail.com>
2025-03-27 11:07:57 +0100

    commit per esercizi 2

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import matplotlib.pyplot as plt
+import numpy as np  
 
-plt.plot()
+np.random.seed(42)  # Per riproducibilità
+arr = np.random.rand(10, 3)  # Array 10x3 con valori casuali tra 0 e 1  
+
+idx = np.abs(arr - 0.5).argmin(axis=1)  
+closest_values = arr[np.arange(arr.shape[0]), idx]  
+
+print(arr)  
+print(closest_values)
